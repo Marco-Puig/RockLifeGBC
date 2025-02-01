bc84ac698e4b5daa39c96b18e0408b5c49dc476f
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2025-02-01 13:42:46 -0500

    Added Audio

--- main.c
+++ main.c
@@ -135,7 +135,21 @@ void main(void)
     // Loop forever
     while(1) {
 		// Game main loop processing goes here
+		
+		// Enable sound - sound registers:
+		NR52_REG = 0x80;
+		NR51_REG = 0xFF;
+		NR50_REG = 0x77;
 
+		// play sound
+		NR10_REG = 0x16;
+		NR11_REG = 0x40;
+		NR12_REG = 0x73;
+		NR13_REG = 0x00;
+		NR14_REG = 0xC3;
+
+		// Wait for sound to finish
+		while (NR52_REG & 0x80);
 
 		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
